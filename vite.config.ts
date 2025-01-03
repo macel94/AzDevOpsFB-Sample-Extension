4b23f406fed2a2437d259a3ce1f6ed8091353597
Francesco Belacca <francesco.belacca@wuerth-phoenix.net>
2025-01-03 10:55:17 +0000

    solving https trust issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl()
-  ],
+  plugins: [react()],
   server: {
-		https: true, // same as "--https" flag
 		host: true, // same as "--host" flag
-	}
+    https: {
+      key: fs.readFileSync('./certificates/key.pem'),
+      cert: fs.readFileSync('./certificates/cert.pem'),
+    },
+  }
 })
